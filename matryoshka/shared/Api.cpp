b8b2174812fe8e2f8a2831940215667b618fdd63
Christopher Gundler <c.gundler@mail.de>
2020-05-28 10:08:40 +0200

    Introduce the C API for interaction with foreign programming languages

---
+++ matryoshka/shared/Api.cpp
@@ -0,0 +1,190 @@
+/*
+This file is part of Matryoshka.
+Copyright (C) 2020 Christopher Gundler <christopher@gundler.de>
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "Api.h"
+
+#include "../data/FileSystem.h"
+
+struct FileSystem {
+  matryoshka::data::FileSystem file_system_;
+  explicit FileSystem(matryoshka::data::FileSystem &&file_system) : file_system_(std::move(file_system)) {}
+};
+
+struct Status {
+  matryoshka::data::Error error_;
+  explicit Status(matryoshka::data::Error error) : error_(error) {}
+};
+
+struct FileHandle {
+  matryoshka::data::File file_;
+  explicit FileHandle(matryoshka::data::File &&file) : file_(std::move(file)) {}
+};
+
+void DestroyFileSystem(FileSystem * file_system) {
+  delete file_system;
+}
+
+void DestroyStatus(Status * status) {
+  delete status;
+}
+
+void DestroyFileHandle(FileHandle * file_handle) {
+  delete file_handle;
+}
+
+template<typename R, typename T>
+inline R *HandleError(Status * *status, T
+value) {
+if (status != nullptr) {
+*
+status = new Status(matryoshka::data::Error(value));
+}
+return nullptr;
+}
+
+template<typename R, typename T>
+inline R *HandleError(Status * *status,
+					  matryoshka::data::sqlite::Result<T, matryoshka::data::sqlite::Status> && value) {
+  if (status != nullptr) {
+	*status = new Status(matryoshka::data::Error(std::get<matryoshka::data::sqlite::Status>(value)));
+  }
+  return nullptr;
+}
+
+template<typename R, typename T>
+inline R *HandleError(Status * *status, matryoshka::data::Result<T> && value) {
+  if (status != nullptr) {
+	*status = new Status(std::get<matryoshka::data::Error>(value));
+  }
+  return nullptr;
+}
+
+FileSystem *Load(const char *path, Status **status) {
+  auto database = matryoshka::data::sqlite::Database::Create(path);
+  if (!database) {
+	return HandleError<FileSystem>(status, std::move(database));
+  }
+
+  auto file_system =
+	  matryoshka::data::FileSystem::Open(std::move(std::get<matryoshka::data::sqlite::Database>(database)));
+  if (!file_system) {
+	return HandleError<FileSystem>(status, std::move(file_system));
+  }
+  return new FileSystem(std::move(std::move(std::get<matryoshka::data::FileSystem>(file_system))));
+}
+
+const char *GetMessage(Status * status) {
+  if (status == nullptr) {
+	return "";
+  }
+  return matryoshka::data::Error::Message(matryoshka::data::Error(status->error_)).data();
+}
+
+FileHandle *Open(FileSystem * file_system,
+const char *path, Status
+**status) {
+if (file_system == nullptr || path == nullptr) {
+return
+HandleError<FileHandle>(status, matryoshka::data::errors::ArgumentError()
+);
+}
+
+auto parsed_path = matryoshka::data::Path(path);
+auto result = file_system->file_system_.Open(parsed_path);
+if (!result) {
+return
+HandleError<FileHandle>(status, std::move(result)
+);
+}
+
+return new
+FileHandle (std::move(std::get<matryoshka::data::File>(result)));
+}
+
+FileHandle *Push(FileSystem * file_system,
+const char *inner_path,
+const char *file_path,
+int chunk_size,
+	Status
+**status) {
+if (file_system == nullptr || inner_path == nullptr || file_path == nullptr) {
+return
+HandleError<FileHandle>(status, matryoshka::data::errors::ArgumentError()
+);
+}
+
+const auto path = matryoshka::data::Path(inner_path);
+auto result = file_system->file_system_.Create(path, file_path, chunk_size);
+if (!result) {
+return
+HandleError<FileHandle>(status, std::move(result)
+);
+}
+
+return new
+FileHandle (std::move(std::get<matryoshka::data::File>(result)));
+}
+
+Status *Pull(FileSystem * file_system, FileHandle * file,
+const char *file_path
+) {
+if (file_system == nullptr || file == nullptr || file_path == nullptr) {
+return new
+Status (matryoshka::data::Error(matryoshka::data::errors::ArgumentError()));
+}
+
+const auto length = file_system->file_system_.Size(file->file_);
+auto result = file_system->file_system_.Read(file->file_, file_path, 0, length);
+return result ? new
+Status(result
+.
+value()
+) : nullptr;
+}
+
+int Find(FileSystem * file_system,
+const char *path,
+void (*callback)(const char *)
+) {
+if (file_system == nullptr) {
+return 0;
+}
+
+std::vector<matryoshka::data::Path> paths;
+if (path == nullptr) {
+file_system->file_system_.
+Find(paths);
+} else {
+const auto parsed_path = matryoshka::data::Path(path);
+file_system->file_system_.
+Find(parsed_path, paths
+);
+}
+
+for (
+auto &found_path
+: paths) {
+callback(found_path
+.
+AbsolutePath()
+.
+c_str()
+);
+}
+return paths.
+size();
+}
+
+int GetSize(FileSystem * file_system, FileHandle * file) {
+  if (file == nullptr || file_system == nullptr) {
+	return 0;
+  }
+  return file_system->file_system_.Size(file->file_);
+}
+
+
---
+++ matryoshka/shared/Api.h
@@ -0,0 +1,108 @@
+/*
+This file is part of Matryoshka.
+Copyright (C) 2020 Christopher Gundler <christopher@gundler.de>
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef MATRYOSHKA_MATRYOSHKA_SHARED_API_H_
+#define MATRYOSHKA_MATRYOSHKA_SHARED_API_H_
+
+#include <matryoshka_export.h>
+
+extern "C" {
+struct FileSystem;
+struct Status;
+struct FileHandle;
+
+/**
+ * Open a SQlite database containing the Matryoshka virtual file system.
+ * @param path The path to the Matryoshka SQlite database.
+ * @param status Contains the error code of the failure if and only if the return value is nullptr. Setting this value to nullptr is safe and will not save the error code.
+ * @return A pointer to the virtual file system or nullptr on failure.
+ */
+MATRYOSHKA_EXPORT FileSystem * Load(const char *path, Status **status);
+
+/**
+ * Destroy a file system.
+ * @param file_system The virtual file system. Passing nullptr is a safe no-op.
+ */
+MATRYOSHKA_EXPORT void DestroyFileSystem(FileSystem * file_system);
+
+/**
+ * Destroy a status.
+ * @param status The status. Passing nullptr is a safe no-op.
+ */
+MATRYOSHKA_EXPORT void DestroyStatus(Status *status);
+
+/**
+ * Destroy a file handle.
+ * @param file_handle The file handle. Passing nullptr is a safe no-op.
+ */
+MATRYOSHKA_EXPORT void DestroyFileHandle(FileHandle *file_handle);
+
+/**
+ * Return the error message associated with a status.
+ * @param status The status of interest.
+ * @return A human-readable description of the failure.
+ */
+MATRYOSHKA_EXPORT const char *GetMessage(Status *status);
+
+/**
+ * Open a existing file on the virtual file system.
+ * @param file_system A pointer to the virtual file system.
+ * @param path The (inner) path on the virtual file system (mind the forward slashes as separators!)
+ * @param status Contains the error code of the failure if and only if the return value is nullptr. Setting this value to nullptr is safe and will not save the error code.
+ * @return A handle to the file or nullptr at failure.
+ */
+MATRYOSHKA_EXPORT FileHandle * Open(FileSystem * file_system , const char *path, Status * * status ) ;
+
+/**
+ * Push a file to the virtual file system.
+ * @param file_system A pointer to the virtual file system.
+ * @param inner_path The inner path on the virtual file system (mind the forward slashes as separators!)
+ * @param file_path The path on the real file system.
+ * @param chunk_size The proposed chunk size. Negative values will let the virtual file system choose.
+ * @param status Contains the error code of the failure if and only if the return value is nullptr. Setting this value to nullptr is safe and will not save the error code.
+ * @return A handle to the newly created file or nullptr on failure.
+ */
+MATRYOSHKA_EXPORT FileHandle * Push(FileSystem * file_system ,
+const char *inner_path,
+const char *file_path,
+int chunk_size,
+	Status * * status ) ;
+
+/**
+ * Pull a file from the database into the virtual file system.
+ * @param file_system A pointer to the virtual file system.
+ * @param inner_path The inner path on the virtual file system (mind the forward slashes as separators!)
+ * @param file_path The path on the real file system.
+ * @return A error ocurring during operation or nullptr on success.
+ */
+MATRYOSHKA_EXPORT Status * Pull(FileSystem * file_system ,
+FileHandle *inner_path,
+const char *file_path ) ;
+
+/**
+ * Search for a specific file(s).
+ * @param file_system A pointer to the virtual file system.
+ * @param path The path supporting glob-like palceholders.
+ * @param callback A callback for each path found.
+ * @return The number of paths found.
+ */
+MATRYOSHKA_EXPORT int Find(FileSystem * file_system,
+const char *path,
+void (*callback)(const char *)) ;
+
+/**
+ * Returns the size of a file.
+ * @param file_system A pointer to the virtual file system.
+ * @param file A handle to the file.
+ * @return File size in bytes.
+ */
+MATRYOSHKA_EXPORT int GetSize(FileSystem * file_system, FileHandle * file);
+
+};
+
+#endif //MATRYOSHKA_MATRYOSHKA_SHARED_API_H_
