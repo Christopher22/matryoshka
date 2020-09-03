328c4c00dc2a9148954d718538575de012d201cf
Christopher Gundler <c.gundler@mail.de>
2020-09-03 14:27:54 +0200

    Optimize building and include version info

--- matryoshka/shared/Api.cpp
+++ matryoshka/shared/Api.cpp
@@ -25,31 +25,30 @@ struct FileHandle {
   explicit FileHandle(matryoshka::data::File &&file) : file_(std::move(file)) {}
 };
 
-void DestroyFileSystem(FileSystem * file_system) {
+void DestroyFileSystem(FileSystem *file_system) {
   delete file_system;
 }
 
-void DestroyStatus(Status * status) {
+void DestroyStatus(Status *status) {
   delete status;
 }
 
-void DestroyFileHandle(FileHandle * file_handle) {
+void DestroyFileHandle(FileHandle *file_handle) {
   delete file_handle;
 }
 
 template<typename R, typename T>
-inline R *HandleError(Status * *status, T
+inline R *HandleError(Status **status, T
 value) {
-if (status != nullptr) {
-*
-status = new Status(matryoshka::data::Error(value));
-}
-return nullptr;
+  if (status != nullptr) {
+	*status = new Status(matryoshka::data::Error(value));
+  }
+  return nullptr;
 }
 
 template<typename R, typename T>
-inline R *HandleError(Status * *status,
-					  matryoshka::data::sqlite::Result<T, matryoshka::data::sqlite::Status> && value) {
+inline R *HandleError(Status **status,
+					  matryoshka::data::sqlite::Result<T, matryoshka::data::sqlite::Status> &&value) {
   if (status != nullptr) {
 	*status = new Status(matryoshka::data::Error(std::get<matryoshka::data::sqlite::Status>(value)));
   }
@@ -57,7 +56,7 @@ inline R *HandleError(Status * *status,
 }
 
 template<typename R, typename T>
-inline R *HandleError(Status * *status, matryoshka::data::Result<T> && value) {
+inline R *HandleError(Status **status, matryoshka::data::Result<T> &&value) {
   if (status != nullptr) {
 	*status = new Status(std::get<matryoshka::data::Error>(value));
   }
@@ -78,109 +77,79 @@ FileSystem *Load(const char *path, Status **status) {
   return new FileSystem(std::move(std::move(std::get<matryoshka::data::FileSystem>(file_system))));
 }
 
-const char *GetMessage(Status * status) {
+const char *GetMessage(Status *status) {
   if (status == nullptr) {
 	return "";
   }
   return matryoshka::data::Error::Message(matryoshka::data::Error(status->error_)).data();
 }
 
-FileHandle *Open(FileSystem * file_system,
-const char *path, Status
-**status) {
-if (file_system == nullptr || path == nullptr) {
-return
-HandleError<FileHandle>(status, matryoshka::data::errors::ArgumentError()
-);
+FileHandle *Open(FileSystem *file_system,
+				 const char *path, Status
+				 **status) {
+  if (file_system == nullptr || path == nullptr) {
+	return HandleError<FileHandle>(status, matryoshka::data::errors::ArgumentError());
+  }
+
+  auto parsed_path = matryoshka::data::Path(path);
+  auto result = file_system->file_system_.Open(parsed_path);
+  if (!result) {
+	return HandleError<FileHandle>(status, std::move(result));
+  }
+
+  return new FileHandle(std::move(std::get<matryoshka::data::File>(result)));
 }
 
-auto parsed_path = matryoshka::data::Path(path);
-auto result = file_system->file_system_.Open(parsed_path);
-if (!result) {
-return
-HandleError<FileHandle>(status, std::move(result)
-);
+FileHandle *Push(FileSystem *file_system,
+				 const char *inner_path,
+				 const char *file_path,
+				 int chunk_size,
+				 Status
+				 **status) {
+  if (file_system == nullptr || inner_path == nullptr || file_path == nullptr) {
+	return HandleError<FileHandle>(status, matryoshka::data::errors::ArgumentError());
+  }
+
+  const auto path = matryoshka::data::Path(inner_path);
+  auto result = file_system->file_system_.Create(path, file_path, chunk_size);
+  if (!result) {
+	return HandleError<FileHandle>(status, std::move(result));
+  }
+
+  return new FileHandle(std::move(std::get<matryoshka::data::File>(result)));
 }
 
-return new
-FileHandle (std::move(std::get<matryoshka::data::File>(result)));
+Status *Pull(FileSystem *file_system, FileHandle *file, const char *file_path) {
+  if (file_system == nullptr || file == nullptr || file_path == nullptr) {
+	return new Status(matryoshka::data::Error(matryoshka::data::errors::ArgumentError()));
+  }
+
+  const auto length = file_system->file_system_.Size(file->file_);
+  auto result = file_system->file_system_.Read(file->file_, file_path, 0, length);
+  return result ? new Status(result.value()) : nullptr;
 }
 
-FileHandle *Push(FileSystem * file_system,
-const char *inner_path,
-const char *file_path,
-int chunk_size,
-	Status
-**status) {
-if (file_system == nullptr || inner_path == nullptr || file_path == nullptr) {
-return
-HandleError<FileHandle>(status, matryoshka::data::errors::ArgumentError()
-);
+int Find(FileSystem *file_system, const char *path, void (*callback)(const char *)) {
+  if (file_system == nullptr) {
+	return 0;
+  }
+
+  std::vector<matryoshka::data::Path> paths;
+  if (path == nullptr) {
+	file_system->file_system_.Find(paths);
+  } else {
+	const auto parsed_path = matryoshka::data::Path(path);
+	file_system->file_system_.Find(parsed_path, paths);
+  }
+
+  for (auto &found_path: paths) {
+	callback(found_path.AbsolutePath().c_str());
+  }
+
+  return paths.size();
 }
 
-const auto path = matryoshka::data::Path(inner_path);
-auto result = file_system->file_system_.Create(path, file_path, chunk_size);
-if (!result) {
-return
-HandleError<FileHandle>(status, std::move(result)
-);
-}
-
-return new
-FileHandle (std::move(std::get<matryoshka::data::File>(result)));
-}
-
-Status *Pull(FileSystem * file_system, FileHandle * file,
-const char *file_path
-) {
-if (file_system == nullptr || file == nullptr || file_path == nullptr) {
-return new
-Status (matryoshka::data::Error(matryoshka::data::errors::ArgumentError()));
-}
-
-const auto length = file_system->file_system_.Size(file->file_);
-auto result = file_system->file_system_.Read(file->file_, file_path, 0, length);
-return result ? new
-Status(result
-.
-value()
-) : nullptr;
-}
-
-int Find(FileSystem * file_system,
-const char *path,
-void (*callback)(const char *)
-) {
-if (file_system == nullptr) {
-return 0;
-}
-
-std::vector<matryoshka::data::Path> paths;
-if (path == nullptr) {
-file_system->file_system_.
-Find(paths);
-} else {
-const auto parsed_path = matryoshka::data::Path(path);
-file_system->file_system_.
-Find(parsed_path, paths
-);
-}
-
-for (
-auto &found_path
-: paths) {
-callback(found_path
-.
-AbsolutePath()
-.
-c_str()
-);
-}
-return paths.
-size();
-}
-
-int GetSize(FileSystem * file_system, FileHandle * file) {
+int GetSize(FileSystem *file_system, FileHandle *file) {
   if (file == nullptr || file_system == nullptr) {
 	return 0;
   }
